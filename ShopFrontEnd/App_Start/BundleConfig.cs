e4fae973fc07fbc7c54566d6faae830b520d38b0
minhtuan520 <minhtuan520@hotmail.com.vn>
2018-08-25 18:05:32 +0700

    Add project files.

---
+++ ShopFrontEnd/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ShopFrontEnd
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {         
+            //------------------------------------------------------------------
+            bundles.Add(new StyleBundle("~/Assest/css").Include("~/Assest/css/style.css", "~/Assest/css/slider.css"));
+
+            bundles.Add(new ScriptBundle("~/Assest/js").Include("~/Assest/js/easing.js", "~/Assest/js/jquery-1.7.2.min.js", "~/Assest/js/move-top.js", "~/Assest/js/startstop-slider.js"));
+        }
+    }
+}
